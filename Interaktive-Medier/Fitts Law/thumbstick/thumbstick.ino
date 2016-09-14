846ffa4e2d06b89ccacfb82227694b9e41757cce
Sophie Mai Chau <sophie_chau@hotmail.com>
2016-09-14 16:18:41 +0200

    added arduino code

---
+++ Interaktive-Medier/Fitts Law/thumbstick/thumbstick.ino
@@ -0,0 +1,18 @@
+int POTY_PIN = 2;
+int button = 6;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(button, INPUT_PULLUP);
+}
+
+void loop() {
+  int val = analogRead(POTY_PIN);
+  int val2 = val/256;
+  int val3 = val%256;
+  int val4 = digitalRead(button);
+  Serial.write(val2);
+  Serial.write(val3);
+  Serial.write(val4);
+  delay(100);
+}
